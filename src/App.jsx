5508205cfadeead06e0790e7e46624e6e3a8252f
Beaskyy <afeez.akintobi@yahoo.com>
2025-02-27 08:13:50 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import './App.css'
+
+const Card = () => {
+    return (
+        <h2>Card component</h2>
+    )
+}
+const App = () => {
+    return (
+        <div>
+        <h2>Functional Arrow component</h2>
+            <Card />
+            <Card />
+            <Card />
+        </div>
+
+    )
+}
+
+export default App
